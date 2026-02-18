3d9ef81ba9695e3d4dd4a1f42641991c6c95cf5c
merafiulhoque <me.rafiulhoque@gmail.com>
2026-02-18 09:00:10 +0530

    fourth commit

--- src/controller/login.ts
+++ src/controller/login.ts
@@ -2,9 +2,12 @@ import { Request, Response } from "express";
 import { loginFormSchema } from "../validators/schemas.js";
 import pool from "../config/db.js";
 import { bcryptCompare } from "../utils/bcryptHash.js";
+import { ApiResponse } from "../type/types.js";
 
 export async function login(req: Request, res: Response){
     try {
+        const role = req.query.role
+        console.log(role)
         const result = loginFormSchema.safeParse(req.body)
         if(!result.success){
             return res.status(400).json({
@@ -14,28 +17,34 @@ export async function login(req: Request, res: Response){
 
         const {email, password} = result.data
 
-        const query = "SELECT password FROM admin WHERE email= $1"
-        const response = await pool.query(query, [email])
+        const query = `SELECT password FROM ${role} WHERE email= $1`
+        const queryRes = await pool.query(query, [email])
 
-        const isPassOk = await bcryptCompare(password, response.rows[0].password as string)
+        const isPassOk = await bcryptCompare(password, queryRes.rows[0].password as string)
 
 
         if(!isPassOk){
-            return res.status(300).json({
+            const response: ApiResponse = {
                 success: false,
-                msg: "Wrong credentials"
-            })
+                message: "Wrong credentials"
+            }
+            return res.status(400).json(response)
         }
 
-        return res.status(200).json({
-                success: true,
-                msg: "Login successfull"
-            })
+        const response: ApiResponse = {
+            success: true,
+            message: "Login Successfull"
+        }
+
+        return res.status(200).cookie("a","a").json(response)
 
     } catch (error) {
-        return res.status(500).json({
-            success: false,
-            msg: "Something went wrong"
-        })
+        if(error instanceof Error){
+            const response: ApiResponse = {
+                success: false,
+                message: error.message
+            }
+            return res.status(500).json(response)
+        }
     }
 }
---
+++ src/helpers/sendApiResponse.ts
@@ -0,0 +1,5 @@
+// import { ApiResponse } from "../type/types.js";
+
+// export function sendResponse<T>{
+
+// }
